17a831cda6befc1e0f9a716c2322071240e5c90f
Ender2397 <pmsmarkes4@gmail.com>
2024-11-21 18:07:05 +0000

    Update Naive_Bayes.m
    UwU

--- Naive_Bayes.m
+++ Naive_Bayes.m
@@ -1,10 +1,11 @@
-data_set=[  "WE", "Roubada";
-            "A LOT", "Não roubada";
-            "asdasdf", "Roubada";
-            "MOOORE", "Não roubada";
-            "MOOOOOOORE", "NEED"];
+data_set=[  "WE", "eletronico", "uso diário";
+            "A LOT", "comestível", "doce";
+            "asdasdf", "comestível", "salgado";
+            "MOOORE", "eletronico", "entretenimento";
+            "MOOOOOOORE", "brinquedo", "entretenimento"];
 
 treino = data_set(1:3,:);
 teste = data_set(4,:);
-classes = unique(data_set(:, 2));
+classes = categorical(data_set(1:end,2:end)')
+classes_unicas = unique(classes)
 model = struct();
