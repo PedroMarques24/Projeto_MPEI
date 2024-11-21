310c275f5970a48e198551cc682bd58f9e83b3c0
Ender2397 <131037469+Ender2397@users.noreply.github.com>
2024-11-21 17:08:57 +0000

    Create Bloom_Filter.m
    ¯\_(ツ)_/¯

---
+++ Bloom_Filter.m
@@ -0,0 +1,10 @@
+n=1000;
+k=3;
+
+filtro=inicializar(n);
+
+alguma_cena='I wanna DIE';
+filtro=inserir(filtro,alguma_cena,k);
+verificar(filtro,alguma_cena,k)
+
+verificar(filtro, 'pls kill me, I m suffering', k)
